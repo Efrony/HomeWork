94e71355f04e21a03048927f4dae13662ae0bcc2
Efrony <efron.vit@gmail.com>
2019-02-19 13:06:11 +0300

    js lesson 2 fin

--- HWjs/script.js
+++ HWjs/script.js
@@ -1,3 +1,4 @@
+/* --------------------------------- ЗАДАНИЕ №2 ---------------------------------*/
 function case2() {
     var a = 2;
     var x = 1 + (a *= 2);
@@ -8,17 +9,25 @@ function randomNumber() {
     return Math.round((Math.random() - 0.5) * 10)
 }
 
+
+/* --------------------------------- ЗАДАНИЕ №3 ---------------------------------*/
 function case3() {
     var a = randomNumber()
     var b = randomNumber()
+    alert('Объявим две случайные переменные.\n Пусть они будут в промежуткt [-5; 5]:\n' + 'a = ' + a + '\n' + 'b = ' + b)
     if (a >= 0 && b >= 0) {
-        alert(Math.max(a, b) - Math.min(a, b))
+        alert('Так как оба числа положительные высчитываем разность: ' + (+Math.max(a, b) - +Math.min(a, b)))
     }
     else if (a < 0 && b < 0) {
-        alert(a * b)
+        alert('Так как оба числа отрицательные высчитываем произведение: ' + (+a * +b))
+    }
+    else {
+        alert('Так как числа разных знаков высчитываем сумму: ' + (+a + +b))
     }
 }
 
+
+/* --------------------------------- ЗАДАНИЕ №4 ---------------------------------*/
 function case4() {
     while (true) {
         var a = +prompt('Введите значение "a" от  0 до 15 включительно')
@@ -28,6 +37,8 @@ function case4() {
     case 0:
         alert(a++)
     case 1:
+        alert(a++)
+    case 2:
         alert(a++)
     case 3:
         alert(a++)
@@ -58,6 +69,8 @@ function case4() {
     }
 }
 
+
+/* --------------------------------- ЗАДАНИЕ №5 ---------------------------------*/
 function casePlus(firstNumber, secondNumber) {
     return firstNumber + secondNumber
 }
@@ -78,12 +91,14 @@ function case5() {
     var d = randomNumber()
     var c = randomNumber()
     alert('На примерe ' + d + ' и ' + c + ' показатели следующие')
-    alert('плюс ' + casePlus(d, c))
-    alert('минус ' + caseMinus(d, c))
-    alert('умножить ' + caseUmnoj(d, c))
-    alert('делить ' + caseDel(d, c))
+    alert('Сложение: ' + casePlus(d, c))
+    alert('Разница: ' + caseMinus(d, c))
+    alert('Произведение: ' + caseUmnoj(d, c))
+    alert('Деление: ' + caseDel(d, c))
 }
 
+
+/* --------------------------------- ЗАДАНИЕ №6 ---------------------------------*/
 function mathOperation(arg1, arg2, operation) {
     switch (operation) {
     case '+':
@@ -102,7 +117,7 @@ function case6() {
     var n = randomNumber()
     var testSwitch = true
     while (testSwitch) {
-        var answerUser = prompt('Сгенерируем два случайных числа. Пусть это будет ' + m + ' и ' + n + '. Введите одну из оперций: + - * /')
+        var answerUser = prompt('Сгенерируем два случайных числа. \n Пусть это будет ' + m + ' и ' + n + '. \n Введите одну из оперций: + - * /')
         switch (answerUser) {
         case '+':
         case '-':
@@ -114,6 +129,42 @@ function case6() {
     alert(m + ' ' + answerUser + ' ' + n + ' = ' + mathOperation(m, n, answerUser))
 }
 
-function case7() {}
 
-function case8() {}
+/* --------------------------------- ЗАДАНИЕ №7 ---------------------------------*/
+function case7() {
+    alert('0 == null: '+ (0 == null) + '\n' + 
+          '0 > null: '+ (0 > null) + '\n' + 
+          '0 < null: '+ (0 < null) + '\n' +
+          '0 === null: '+ (0 === null) + '\n' +
+          '0 != null: '+ (0 != null) + '\n' +
+          '0 <= null: '+ (0 <= null) + '\n' + 
+          '0 >= null: '+ (0 >= null) + '\n' +
+         'WHAT !?!? Как я понял, это связано с преобразованием типов этих значений при сравнивании различными знаками. Сильно углубляться не стал, там дебри =)')
+}
+
+
+/* --------------------------------- ЗАДАНИЕ №8 ---------------------------------*/
+function power(val, pow) {
+    if (pow >= 0) {
+        if (pow == 0) return 1
+        else if (pow == 1) return val
+        else {
+            pow--
+            return val * power(val, pow)
+        }
+    } else {
+        if (pow == -1) return 1/val
+        else {
+            pow++
+            return 1/val * power(val, pow)
+        }
+    }
+}
+
+
+function case8() {
+    var x = randomNumber()
+    var y = randomNumber()
+    alert('Сгенерируем два случайных числа. \n Пусть это будет ' + x + ' в степени ' + y)
+    alert('Ответ: ' + power(x, y))
+}
