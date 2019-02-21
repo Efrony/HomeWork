068a61a33d05ac02b372f528c6b4d04172665454
Efrony <efron.vit@gmail.com>
2019-02-21 17:28:31 +0300

    7

--- 3HWjs/script.js
+++ 3HWjs/script.js
@@ -2,7 +2,9 @@
 var primeNumber = []
 
 function case1() {
-    for (var number = 1; number <= 100; number++) {
+    var number = 0
+    while (number <= 100) {
+        number++
         var testPrime = 0
         for (var digit = 1; digit <= 100; digit++) {
             if (number >= digit) {
@@ -24,7 +26,37 @@ function case1() {
 /* --------------------------------- ЗАДАНИЕ №2 ---------------------------------*/
 function case2() {}
 /* --------------------------------- ЗАДАНИЕ №3 ---------------------------------*/
-function case3() {}
+
+var cart = []
+
+function write(text) {
+    document.getElementById("cart").innerHTML = text;
+}
+
+function jersey() {
+    cart.push(['jersey',800,1])
+}
+
+function pants() {
+    cart.push(['pants',1500,1])
+}
+
+function sweatshirt() {
+    cart.push(['sweatshirt',2000,1])
+}
+
+
+function countBasketPrice() {
+    var count = 0
+    var sumPrice = 0
+
+    for (var i in cart) {
+        sumPrice += cart[i][1]
+        count += cart[i][2]
+    }
+    alert (count + ' ' + sumPrice)
+    
+}
 /* --------------------------------- ЗАДАНИЕ №4 ---------------------------------*/
 function case4() {}
 /* --------------------------------- ЗАДАНИЕ №5 ---------------------------------*/
