379cae5102de4558b999eb7b4bdd942ed6e6c1ac
Efrony <efron.vit@gmail.com>
2019-03-01 15:23:34 +0300

    HWjs-5 final

--- HWjs-5/script.js
+++ HWjs-5/script.js
@@ -2,15 +2,13 @@
 var $chessBoard = document.getElementById('chessBoard')
 var $table = document.createElement('table')
 var $blackIndicator = false
-
-/* --- Функция создания child с указанием тега, контента, класса стилей и родителя----*/
+    /* --- Функция создания child с указанием тега, контента, класса стилей и родителя----*/
 function createChild(teg, content, classStyle, parent) {
     var $elem = document.createElement(teg)
     $elem.textContent = content
     $elem.classList.add(classStyle)
     parent.appendChild($elem)
 }
-
 /* ------ Верхний буквенный ряд с пробелами побокам ------ */
 var $tr = document.createElement('tr')
 createChild('td', '', 'borderStyle', $tr)
@@ -19,8 +17,7 @@ for (var j = 65; j <= 72; j++) {
 }
 createChild('td', '', 'borderStyle', $tr)
 $table.appendChild($tr)
-
-/* ------------------------- Доска  ---------------------- */
+    /* ------------------------- Доска  ---------------------- */
 for (var i = 8; i > 0; i--) {
     var $tr = document.createElement('tr')
     createChild('td', i, 'borderStyle', $tr) /* левая номерная колонка */
@@ -40,7 +37,6 @@ for (var i = 8; i > 0; i--) {
     $table.appendChild($tr)
     $blackIndicator = !$blackIndicator
 }
-
 /* ------ Нижний буквенный ряд с пробелами побокам ------ */
 var $tr = document.createElement('tr')
 createChild('td', '', 'borderStyle', $tr)
@@ -48,53 +44,26 @@ for (var j = 65; j <= 72; j++) {
     createChild('td', String.fromCharCode(j), 'borderStyle', $tr)
 }
 createChild('td', '', 'borderStyle', $tr)
-$table.appendChild($tr) 
-
-/*-------убираем отступы таблицы---------*/
-$table.classList.add('noBorderTable') 
+$table.appendChild($tr)
+    /*-------убираем отступы таблицы---------*/
+$table.classList.add('noBorderTable')
 $chessBoard.appendChild($table)
 
 
-/* --------------------------------- ЗАДАНИЕ №2 ---------------------------------*/
-var cart = []
-var product = [
-    {
-        name: 'Футболка'
-        , price: 800
-        , count: 1
-        , color: 'red'
-        , size: 'S'
-        }
-        , {
-        name: 'Штаны'
-        , price: 1500
-        , count: 1
-        , color: 'black'
-        , size: 'M'
-        }
-        , {
-        name: 'Толстовка'
-        , price: 2000
-        , count: 1
-        , color: 'blue'
-        , size: 'L'
-        }
-]
-/* --------------- Вывод результата в виде текста.------------*/
 
+    /* --------------------------------- ЗАДАНИЕ №2 ---------------------------------*/
+    /* --------------- Вывод результата в виде текста.------------*/
 function messageCart(text) {
     document.getElementById('cart').textContent = text
 }
-
-messageCart('В корзине пусто. Добавьте товар и рассчитайте стоимость.')
-
-/* ----------------------- Подсчёт суммы --------------------*/
+messageCart('В корзине пусто.')
+    /* ----------------------- Подсчёт суммы --------------------*/
 function countBasketPrice(cartUser) {
     var countCart = 0
     var sumPriceCart = 0
     for (var i = 0; i < cartUser.length; i++) {
-        sumPriceCart += cartUser[i].price
-        countCart += cartUser[i].count
+        sumPriceCart += cartUser[i]._price
+        countCart += cartUser[i]._count
     }
     messageCart('Итого: ' + countCart + ' шт. стоимостью ' + sumPriceCart + ' рублей. ')
 }
@@ -104,31 +73,84 @@ function addToCart(goods, cartUser) {
     countBasketPrice(cartUser)
 }
 /* ------------------  Очистка корзины ----------------------*/
+
 function clearCart(cartUser) {
-    messageCart('В корзине пусто. Добавьте товар и рассчитайте стоимость.')
+    messageCart('В корзине пусто.')
     cartUser.splice(0, cartUser.length)
 }
 
-/* --------------------------------- ЗАДАНИЕ №3 ---------------------------------*/
+var $clear = document.getElementById('clear')
+$clear.setAttribute('onclick', 'clearCart(cart)')
 
-function catalogVisual(productItem,i) {
+
+/* ------------------------------------- ЗАДАНИЕ №3 ---------------------------------------*/
+var cart = []
+var product = [
+        {
+            'Название товара': 'Футболка'
+            , _price: 800
+            , _count: 1
+            , Цвет: 'red'
+            , Размер: 'S'
+        }
+        , {
+            'Название товара': 'Штаны'
+            , _price: 1500
+            , _count: 1
+            , Цвет: 'black'
+            , Размер: 'M'
+        }
+        , {
+            'Название товара': 'Толстовка'
+            , _price: 2000
+            , _count: 1
+            , Цвет: 'blue'
+            , Размер: 'L'
+        }, {
+            'Название товара': 'Футболка'
+            , _price: 800
+            , _count: 1
+            , Цвет: 'red'
+            , Размер: 'M'
+        }
+        , {
+            'Название товара': 'Штаны'
+            , _price: 1500
+            , _count: 1
+            , Цвет: 'red'
+            , Размер: 'M'
+        }, {
+            'Название товара': 'Штаны'
+            , _price: 1500
+            , _count: 1
+            , Цвет: 'red'
+            , Размер: 'M'
+        }
+
+]
+    /* ----Функция визуализирующая один объект каталога----*/
+function catalogVisualItem(productItem, indexItem) {
+    /* ------- Вывод атрибутов объекта без префикса '_' -------*/
     var content = ''
     for (j in productItem) {
-        content +=  j + ': ' + productItem[j] +'<br>'
+        if (j[0] == '_') continue
+        content += j + ': ' + productItem[j] + '<br>'
     }
     var $elem = document.createElement('div')
     $elem.innerHTML = content
     $elem.classList.add('productItem')
     catalog.appendChild($elem)
-    
-    var addToCartAtribute = ('addToCart(product[' + i + '],cart)')
+        /* -------- Кнопка объекта "Добавить"  --------*/
+    var addToCartAtribute = ('addToCart(product[' + indexItem + '],cart)')
     var $button = document.createElement('button')
-    $button.textContent = 'Добавить в корзину '+ productItem['price'] + 'руб.'
+    $button.textContent = 'Добавить в корзину ' + productItem['_price'] + 'руб.'
     $button.setAttribute('onclick', addToCartAtribute);
     $elem.appendChild($button)
 }
-
-
-for (i = 0; i < product.length; i++) {
-    catalogVisual(product[i],i)
+/* ---- Функция визуализирующая все объекты каталога----*/
+function catalogVisual(productObject) {
+    for (i = 0; i < productObject.length; i++) {
+        catalogVisualItem(productObject[i], i)
+    }
 }
+catalogVisual(product)
