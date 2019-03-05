f348d460bb1d30cb0439fa60465c28944f85b89f
Efrony <efron.vit@gmail.com>
2019-03-05 13:12:36 +0300

    HWjs-6

--- HWjs-6/script.js
+++ HWjs-6/script.js
@@ -1,65 +1,62 @@
-
 var cart = []
 var product = [
         {
-            'Название товара': 'Футболка',
-            _name: 'jersey',
-            _price: 800,
-            _count: 1,
-            _color: 'red',
-            Цвет: 'Красный',
-            Размер: 'S'
+            'Название товара': 'Футболка'
+            , _name: 'jersey'
+            , _price: 800
+            , _count: 1
+            , _color: 'red'
+            , Цвет: 'Красный'
+            , Размер: 'S'
         }, {
-            'Название товара': 'Штаны',
-            _name: 'pants',
-            _price: 1500,
-            _count: 1,
-            _color: 'black',
-            Цвет: 'Чёрный',
-            Размер: 'M'
+            'Название товара': 'Штаны'
+            , _name: 'pants'
+            , _price: 1500
+            , _count: 1
+            , _color: 'black'
+            , Цвет: 'Чёрный'
+            , Размер: 'M'
         }, {
-            'Название товара': 'Толстовка',
-            _name: 'sweatshirt',
-            _price: 2000,
-            _count: 1,
-            _color: 'blue',
-            Цвет: 'Синий',
-            Размер: 'L'
+            'Название товара': 'Толстовка'
+            , _name: 'sweatshirt'
+            , _price: 2000
+            , _count: 1
+            , _color: 'blue'
+            , Цвет: 'Синий'
+            , Размер: 'L'
         }, {
-            'Название товара': 'Футболка',
-            _name: 'jersey',
-            _price: 800,
-            _count: 1,
-            _color: 'black',
-            Цвет: 'Черный',
-            Размер: 'M'
+            'Название товара': 'Футболка'
+            , _name: 'jersey'
+            , _price: 800
+            , _count: 1
+            , _color: 'black'
+            , Цвет: 'Черный'
+            , Размер: 'M'
         }, {
-            'Название товара': 'Толстовка',
-            _name: 'sweatshirt',
-            _price: 1500,
-            _count: 1,
-            _color: 'red',
-            Цвет: 'Красный',
-            Размер: 'M',
-        }, {
-            'Название товара': 'Штаны',
-            _name: 'pants',
-            _price: 1500,
-            _count: 1,
-            _color: 'green',
-            Цвет: 'Зеленый',
-            Размер: 'M',
-        }
+            'Название товара': 'Толстовка'
+            , _name: 'sweatshirt'
+            , _price: 1500
+            , _count: 1
+            , _color: 'red'
+            , Цвет: 'Красный'
+            , Размер: 'M'
+        , }, {
+            'Название товара': 'Штаны'
+            , _name: 'pants'
+            , _price: 1500
+            , _count: 1
+            , _color: 'green'
+            , Цвет: 'Зеленый'
+            , Размер: 'M'
+        , }
 
 ]
-/* ------------------------------- ЗАДАНИЕ №1 ---------------------------*/
-
+    /* ------------------------------- ЗАДАНИЕ №1 ---------------------------*/
     /* --------------- Вывод результата в виде текста.------------*/
 function messageCart(text) {
     document.getElementById('cart').textContent = text
 }
-
-    /* ----------------------- Подсчёт суммы --------------------*/
+/* ----------------------- Подсчёт суммы --------------------*/
 function countBasketPrice(cartUser) {
     var countCart = 0
     var sumPriceCart = 0
@@ -75,20 +72,19 @@ function addToCart(goods, cartUser) {
     countBasketPrice(cartUser)
 }
 /* ------------------  Очистка корзины ----------------------*/
-
 function clearCart(cartUser) {
     messageCart('В корзине пусто.')
     cartUser.length = 0 /*cartUser.splice(0, cartUser.length)*/
 }
+
 function handleClearCart() {
     clearCart(cart)
 }
-
 /*$clear.setAttribute('onclick', 'clearCart(cart)')*/
 
-/* ------------------------------- ЗАДАНИЕ №2 ---------------------------*/
 
-    /* ----Функция визуализирующая один объект каталога----*/
+/* ------------------------------- ЗАДАНИЕ №2 ---------------------------*/
+/* ----Функция визуализирующая один объект каталога----*/
 function catalogVisualItem(productItem, indexItem) {
     /* ------- Вывод атрибутов объекта без префикса '_' -------*/
     var content = ''
@@ -99,6 +95,7 @@ function catalogVisualItem(productItem, indexItem) {
     var $elem = document.createElement('div')
     $elem.innerHTML = content
     $elem.classList.add('productItem')
+    $elem.setAttribute('tabindex', indexItem)
     catalog.appendChild($elem)
         /* -------- Кнопка объекта "Купить"  --------*/
     var $button = document.createElement('button')
@@ -106,13 +103,12 @@ function catalogVisualItem(productItem, indexItem) {
     var addToCartAtribute = ('addToCart(product[' + indexItem + '],cart)')
     $button.setAttribute('onclick', addToCartAtribute)
     $elem.appendChild($button)
-    /* -------- Маленькая картинка в каталоге  --------*/
+        /* -------- Маленькая картинка в каталоге  --------*/
     var $smallImg = document.createElement('img')
     $smallImg.classList.add('smallImg')
     var src = 'img/small/' + productItem['_name'] + '_' + productItem['_color'] + '.jpg'
     $smallImg.src = src
     $elem.insertBefore($smallImg, $button)
-    
 }
 /* ---- Функция визуализирующая все объекты каталога----*/
 function catalogVisual(productObject) {
@@ -121,21 +117,18 @@ function catalogVisual(productObject) {
     }
 }
 /* ---- Функция отображения большого изображения в блоке ----*/
-
 function changeBigPicture(event) {
     var $previev = document.getElementById('previev')
     $previev.innerHTML = ''
-    var $eventElement = event.target
-    
-    var srcList = $eventElement.src.split('/')
-    var src = ''
-    srcList[srcList.indexOf('small')] = 'big'
-
-    
-    
+    if (event.target.tagName === 'IMG') var $eventElement = event.target
+    else if (event.target.tagName === 'DIV') var $eventElement = event.target.children[3]
+    else if (event.target.tagName === 'BUTTON') var $eventElement = event.target.parentElement.children[3]
+    var srcEnd = $eventElement.src.split('/').pop()
+    var src = 'img/big/' + srcEnd
+    var $bigImg = document.createElement('img')
+    $bigImg.src = src
+    $previev.appendChild($bigImg)
 }
-
-
 
 function init() {
     messageCart('В корзине пусто.')
@@ -143,10 +136,9 @@ function init() {
     $clear.textContent = 'Очистить корзину'
     $clear.addEventListener('click', handleClearCart)
     catalogVisual(product)
-    var images = document.getElementsByClassName("smallImg")
-    for (var i = 0; i < images.length; i++) {
-        images[i].onclick = changeBigPicture
+    var imagesItem = document.getElementsByClassName("productItem")
+    for (var j = 0; j < imagesItem.length; j++) {
+        imagesItem[j].onclick = changeBigPicture
     }
 }
-
 window.addEventListener('load', init)
