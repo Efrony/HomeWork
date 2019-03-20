5f47aa54f0903844bbcb85d61d15dfbd7d70a784
Efrony <efron.vit@gmail.com>
2019-03-20 16:02:29 +0300

    генерация разметки списка товаров через классы

--- script/script.js
+++ script/script.js
@@ -9,20 +9,45 @@ var product = [
     {article: '000008', name: 'Mango People Sweatshirt', price: 45}
 ]
 
+class CreateProductItem {  //класс, создающий объект с указанными свойствами продукта и с методом renderHtml, который возвращающает разметку
+    constructor(article, name, price) {
+        this.article = article
+        this.name = name
+        this.price = price
+    }
+    renderHtml() { 
+        return `<figure class="productItem">
+                    <img src="img/product/${this.article}.jpg" alt="productFoto">
+                    <div class="shadowHover">
+                        <button class="addToCart">&ensp;Add to Cart</button>
+                    </div>
+                    <figcaption>${this.name}<p>$ ${this.price}</p></figcaption>
+                    </figure>`
+    }
 
-var createProductItem = (article, name, price) => {return `<figure class="productItem"><img src="img/product/${article}.jpg" alt="productFoto"><div class="shadowHover"><button class="addToCart">&ensp;Add to Cart</button></div><figcaption>${name}<p>$ ${price}</p></figcaption></figure>`}
+}
 
+class CreateProductList { // класс, создающий объект (экземпляр) с методом полученния массива товаров с сервера, и методом вывода этого списка по одному товару через CreateProductItem.renderHtml
+    constructor() {
+        this.productList = [] 
+    }
+    getListServer() {
+        this.productList = product
+    }
+    сreateHtml() {
+        var htmlString = '';
+         this.productList.forEach(function(productItem) { //для каждого из массива выполнить функцию...
+             var productItem = new CreateProductItem(productItem.article, productItem.name, productItem.price)  // на основе класса CreateProductItem создать объект (экземпляр) и присвоить его переменной productItem
+            htmlString += productItem.renderHtml()  // у только что созданного эксемпляра взять разметку и присвоить его строке html 
+         })
+        document.getElementById('product').innerHTML = htmlString 
+    }
+}
 
-var createProductList = list =>{
-    var productList = list.map(item => createProductItem(item.article, item.name, item.price))
-    document.getElementById('product').innerHTML = productList.join('')
-} 
-
-
-   
 function init () {
-    createProductList(product)
-
+    const createProducs = new CreateProductList() // создаём экземпляр класса создающего список товара
+    createProducs.getListServer() // вызываем у этого экземпляра метод получающий список товаров с сервера 
+    createProducs.сreateHtml() // вызываем метод генерирующий разметку каждого товара поочерёдно
 } 
 
 
