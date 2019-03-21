72a0b293b74826adeacdb5798e245a8710631d0e
Efrony <efron.vit@gmail.com>
2019-03-21 12:35:44 +0300

    визуализация товаров корзины подсчёт суммы корзины

--- script/script.js
+++ script/script.js
@@ -32,26 +32,20 @@ class CreateProductItem {  //класс, создающий объект с ук
                     <figcaption>${this.name}<p>$ ${this.price}</p></figcaption>
                     </figure>`
     }
-    renderHtmlCart() {  // разметку элемента в корзине
-        return `<figure>
-                    <img src="img/product/${this.article}.jpg" alt="productFoto">
-                    <figcaption>${this.name}<p>$ ${this.price}</p><p>$ ${this.count}</p></figcaption>
-                </figure>`
-    }
 }
 
 class CreateProductList { // класс, создающий объект (экземпляр) с методом полученния массива товаров с сервера, и методом вывода этого списка по одному товару через CreateProductItem.renderHtml
     constructor() {
         this.productList = [] 
-        this.cartList = []
     }
     getProductListServer() {
         this.productList = product  //запрашиваем товары для каталога
+        this.cartList = cart  //запрашиваем товары для корзины
     }
-    getCartListServer() {
-        this.cartList = cart // //запрашиваем товары для корзины
+    sumCart() { // суммарная стоимость всех продуктов в корзине
+        var sumPrice = this.cartList.reduce(function(sum, item) {
+            return sum + (item.price * item.count)}, 0)
     }
-    /*----------------------взависимости от списка создётся каталог или корзина----------------*/
     сreateHtmlCatalog() {    
         var htmlString = '';
          this.productList.forEach(function(productItem) { //для каждого из массива выполнить функцию...
@@ -60,27 +54,12 @@ class CreateProductList { // класс, создающий объект (экз
             })
         document.getElementById('product').innerHTML = htmlString 
     }
-    сreateHtmlCart() {    
-        var htmlString = '';
-         this.cartList.forEach(function(productItem) { 
-                var productItem = new CreateProductItem(productItem.article, productItem.name, productItem.price, productItem.count)  
-                htmlString += productItem.renderHtmlCart() 
-            })
-        document.getElementById('cart').innerHTML = htmlString 
-    }
 }
-
-
-
-
-
 
 function init () {
     const createProducs = new CreateProductList() // создаём экземпляр класса создающего список товара
     createProducs.getProductListServer() // вызываем у этого экземпляра метод получающий список товаров с сервера 
     createProducs.сreateHtmlCatalog() // вызываем метод генерирующий разметку каждого товара поочерёдно
-    createProducs.getCartListServer()
-    createProducs.сreateHtmlCart()
 } 
 
 window.addEventListener('load', init)
